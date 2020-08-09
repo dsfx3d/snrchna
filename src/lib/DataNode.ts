fd6d06f5b6b45d758b41e8cd7c5eb6db0b0b5639
Yashodhan Singh <yashodhan.singh@unoapp.io>
2020-08-10 02:53:43 +0530

    :art: move DataNode.ts

---
+++ src/lib/DataNode.ts
@@ -0,0 +1,39 @@
+import { IDataNode } from '../types/IDataNode'
+import { equals } from '../utils/equals'
+
+export class DataNode<T, P = any> implements IDataNode<T, P> {
+  private _data: T
+  private _pointers: P
+
+  constructor(data: T) {
+    this._data = data
+    this._pointers = ({} as unknown) as P
+  }
+
+  get data(): T {
+    return this._data
+  }
+
+  public get pointers(): P {
+    return (new Proxy(
+      (this._pointers as unknown) as object,
+      pointerProxyHandler
+    ) as unknown) as P
+  }
+
+  /**
+   * Compares with a node for equality.
+   *
+   * @param node The which is to be compared.
+   * @returns true if both nodes are equal otherwise false.
+   */
+  equals(node: any): boolean {
+    return equals(this, node)
+  }
+}
+
+const pointerProxyHandler = {
+  set(_target: any, prop: any, _receiver: any) {
+    throw new Error(`property ${String(prop)} cannot be set.`)
+  }
+}
