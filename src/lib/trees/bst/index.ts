5427441089b3945a50e23e2e889675dd5bd829ea
dsfx3d <dsfx3d@gmail.com>
2020-11-23 19:01:50 +0530

    :sparkles: (BST) add traversal policies
    - in order
    - post order
    - level order
    - depth first

--- src/lib/trees/bst/index.ts
+++ src/lib/trees/bst/index.ts
@@ -2,9 +2,12 @@ import { BSTNode } from "./BSTNode";
 
 export class BST<T> {
   root: BSTNode<T>
+  totalNodes: number
 
   constructor(root: T) {
-    this.root = new BSTNode(root)
+    if (root) {
+      this.root = new BSTNode(root)
+    }
   }
 
   insert(...values: T[]) {
@@ -15,6 +18,17 @@ export class BST<T> {
     switch(policy) {
       case TraversalPolicy.PRE_ORDER:
         this.preOrder(this.root, cb)
+        break
+      case TraversalPolicy.IN_ORDER:
+        this.inOrder(this.root, cb)
+        break
+      case TraversalPolicy.POST_ORDER:
+        this.postOrder(this.root, cb)
+      case TraversalPolicy.LEVEL_ORDER:
+        this.levelOrder([this.root], cb)
+        break
+      case TraversalPolicy. DEPTH_FIRST:
+        this.depthFirst([this.root], cb)
         break
     }
   }
@@ -34,6 +48,78 @@ export class BST<T> {
       this.preOrder(subTree.rightChild, cb)
     }
   }
+
+  private inOrder(subTree: BSTNode<T>, cb: Function) {
+    if (subTree === null) {
+      return
+    }
+
+    if (subTree.leftChild) {
+      this.inOrder(subTree.leftChild, cb)
+    }
+
+    cb(subTree)
+
+    if (subTree.rightChild) {
+      this.inOrder(subTree.rightChild, cb)
+    }
+  }
+
+  private postOrder(subTree: BSTNode<T>, cb: Function) {
+    if (subTree === null) {
+      return
+    }
+
+    if (subTree.leftChild) {
+      this.postOrder(subTree.leftChild, cb)
+    }
+
+    if (subTree.rightChild) {
+      this.postOrder(subTree.rightChild, cb)
+    }
+
+    cb(subTree)
+  }
+
+  private levelOrder(nodeQueue: BSTNode<T>[], cb: Function) {
+    if (nodeQueue.length === 0) {
+      return
+    }
+
+    const node = nodeQueue.shift()
+
+    cb(node)
+
+    if (node.leftChild) {
+      nodeQueue.push(node.leftChild)
+    }
+
+    if (node.rightChild) {
+      nodeQueue.push(node.rightChild)
+    }
+
+    this.levelOrder(nodeQueue, cb)
+  }
+
+  private depthFirst(nodeStack: BSTNode<T>[], cb: Function) {
+    if (nodeStack.length === 0) {
+      return
+    }
+
+    const node = nodeStack.pop()
+
+    cb(node)
+
+    if (node.leftChild) {
+      nodeStack.push(node.leftChild)
+    }
+
+    if (node.rightChild) {
+      nodeStack.push(node.rightChild)
+    }
+
+    this.depthFirst(nodeStack, cb)
+  }
 }
 
 export enum TraversalPolicy {
@@ -41,25 +127,5 @@ export enum TraversalPolicy {
   IN_ORDER,
   POST_ORDER,
   LEVEL_ORDER,
-  CUSTOM
-}
-
-function levelOrderPolicy<T>(nodeQueue: BSTNode<T>[], cb: Function) {
-  if (nodeQueue.length === 0) {
-    return
-  }
-
-  const node = nodeQueue.shift()
-
-  if (node.leftChild) {
-    nodeQueue.push(node.leftChild)
-  }
-
-  if (node.rightChild) {
-    nodeQueue.push(node.rightChild)
-  }
-
-  cb(node)
-
-  levelOrderPolicy(nodeQueue, cb)
+  DEPTH_FIRST
 }
--- test/trees/bst.spec.ts
+++ test/trees/bst.spec.ts
@@ -2,15 +2,15 @@ import { BST, TraversalPolicy } from '../../src/lib/trees/bst'
 import { BSTNode } from '../../src/lib/trees/bst/BSTNode'
 
 describe('BST', () => {
-  it('foo', () => {
-    const tree = new BST(12)
-    tree.insert(13, 14, 5, 8, 9, 16, 20, 3, 2)
+  it('can insert nodes', () => {
+    const tree = new BST(8)
+    tree.insert(3, 10, 1, 6, 14, 4, 7, 13)
 
-    const preOrder = []
-    tree.traverse(TraversalPolicy.PRE_ORDER, (node: BSTNode<number>) => {
-      preOrder.push(`${node.data}(${node.leftChild?.data}, ${node.rightChild?.data})`);
+    const depthFirst = []
+    tree.traverse(TraversalPolicy.DEPTH_FIRST, (node: BSTNode<number>) => {
+      depthFirst.push(node.data);
     })
 
-    console.log(preOrder);
+    console.log(depthFirst);
   })
 })
